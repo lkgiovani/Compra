8cd31888be5b0229ee8f3efcf69d935499b361f8
giovani souza <giovani.souza@insidecode.io>
2024-11-22 20:52:02 -0300

    up

--- internal/app/api/model/product_model/product_model.go
+++ internal/app/api/model/product_model/product_model.go
@@ -1,7 +1,9 @@
 package product_model
 
 type Product struct {
-	ID       uint   `json:"id"`
-	Name     string `json:"name"`
-	Quantity int    `json:"quantity"`
+	ID       uint    `json:"id"`
+	Name     string  `json:"name"`
+	Quantity int     `json:"quantity"`
+	Price    float32 `json:"price"`
+	UrlImage string  `json:"urlImage"`
 }
--- pkg/env/env.go
+++ pkg/env/env.go
@@ -3,7 +3,6 @@ package env
 import (
 	"fmt"
 	"os"
-	"strconv"
 )
 
 func GetEnvOrDie(key string) string {
@@ -12,22 +11,6 @@ func GetEnvOrDie(key string) string {
 	if value == "" {
 		err := fmt.Errorf("Missing environment variable %s", key)
 		panic(err)
-	}
-
-	return value
-}
-
-func GetEnvOrDieAsInt(key string) int {
-	valueStr := os.Getenv(key)
-	if valueStr == "" {
-		fmt.Printf("Environment variable %s not set\n", key)
-		os.Exit(1)
-	}
-
-	value, err := strconv.Atoi(valueStr)
-	if err != nil {
-		fmt.Printf("Error converting %s to int: %v\n", key, err)
-		os.Exit(1)
 	}
 
 	return value
